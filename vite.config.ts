e1370ead8d8234d17a1f9bdddbed798d40cac25c
gayatrivenugopal <gayatrivenugopal3@gmail.com>
2025-06-13 16:51:48 +0530

    Updated config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import path from "path";
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -9,6 +11,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react()],
+  base: '/venugopalmenon.github.io/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
