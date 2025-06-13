55d9b704a984817280f336f3bafebc7f1b183b88
gayatrivenugopal <gayatrivenugopal3@gmail.com>
2025-06-13 17:23:20 +0530

    Fixed a bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
-import { Sonner } from "@/components/ui/sonner"; 
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter, Routes, Route } from "react-router-dom";
