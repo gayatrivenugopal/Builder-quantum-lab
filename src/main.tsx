c457db68a97c31483d6d0d6d9e70a188b8f029d2
gayatrivenugopal <gayatrivenugopal3@gmail.com>
2025-06-13 17:17:30 +0530

    Fixed errors

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
