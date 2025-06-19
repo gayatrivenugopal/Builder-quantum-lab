4f0e88e3afa0e74241d9d2d3b3ce44e75a41616e
gayatrivenugopal <gayatrivenugopal3@gmail.com>
2025-06-19 16:55:05 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
